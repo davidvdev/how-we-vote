feebcabb39b6046eb96ed41aef179ccdeaf26f43
davidvdev <david@davidvdev.com>
2021-12-14 16:46:20 -0600

    created Ballot component

---
+++ src/components/Ballot/Ballot.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Ballot = () => {
+    return(
+        <div className="Ballot">
+            Ballot
+        </div>
+    )
+}
+
+export default Ballot
--- src/components/System/System.jsx
+++ src/components/System/System.jsx
@@ -1,12 +1,22 @@
+import './System.css'
+import Ballot from '../Ballot/Ballot'
 
 const System = ({sysName}) => {
     return(
         <main>
             <h2>{sysName}</h2>
+            <h3>What is {sysName} Voting?</h3>
             {/* explanation of the system */}
+
+            <h3>What Countries use {sysName} Voting?</h3>
             {/* what countries use it */}
+
+            <h3>data vis</h3>
             {/* data vis of its popularity */}
+
             {/* sample ballot component */}
+            <Ballot />
+
             {/* related links to continued learning? */}
         </main>
     )
